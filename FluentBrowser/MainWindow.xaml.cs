69796a4b30d538d85c7861e48832f0a919a8857f
gnh1201 <gnh1201@gmail.com>
2020-12-29 21:01:39 +0900

    first commit

---
+++ FluentBrowser/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Fluent;
+
+namespace FluentBrowser
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
