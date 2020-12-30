b3a882125a56b49490f99d8d5bf36f16a8ab060a
gnh1201 <gnh1201@gmail.com>
2020-12-30 19:06:04 +0900

    fix

--- FluentBrowser/MainWindow.xaml.cs
+++ FluentBrowser/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using Fluent;
+using System.Windows.Controls;
+using CefSharp.Wpf;
+using Gecko;
+using System.Windows.Forms.Integration;
+using System.IO;
+using System.Reflection;
 
 namespace FluentBrowser
 {
@@ -10,6 +16,52 @@ namespace FluentBrowser
         public MainWindow()
         {
             InitializeComponent();
+            CreateChromiumTab();
+        }
+
+        private void CreateChromiumTab()
+        {
+            var browser = new ChromiumWebBrowser("m.naver.com");
+            browser.AddressChanged += (sender, e) =>
+            {
+                URLText.Text = browser.Address;
+            };
+
+            var tab = new TabItem();
+            tab.Header = "Chromium";
+            tab.Content = browser;
+            BrowserTab.Items.Add(tab);
+            tab.Focus();
+        }
+
+        private void CreateGekcoTab()
+        {
+            Xpcom.Initialize("Firefox64");
+
+            var browser = new GeckoWebBrowser();
+            browser.Navigate("m.naver.com");
+            browser.LocationChanged += (sender, e) => {
+                URLText.Text = browser.Url.AbsoluteUri;
+            };
+
+            WindowsFormsHost whost = new WindowsFormsHost();
+            whost.Child = browser;
+
+            var tab = new TabItem();
+            tab.Header = "Gekco";
+            tab.Content = whost;
+            BrowserTab.Items.Add(tab);
+            tab.Focus();
+        }
+
+        private void Button_Click_Chromium(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CreateChromiumTab();
+        }
+
+        private void Button_Click_Gekco(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CreateGekcoTab();
         }
     }
 }
